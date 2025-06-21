02d274d8dc31fdd999265a65dff277e1da0a55c1
seperool <seperool@gmail.com>
2025-06-21 18:10:39 -0300

    Lista de seleção de método

--- tela-root.py
+++ tela-root.py
@@ -1,4 +1,5 @@
-from tkinter import * # Importa classes e funções do Tkinter
+from tkinter import *                                                                   # Importa classes e funções do Tkinter
+from tkinter import ttk                                                                 # Importa o módulo ttk do Tkinter (para widgets estilizados)
 
 root = Tk()                                                                             # Cria a janela principal da aplicação
 
@@ -11,7 +12,7 @@ class aplicativo():                                                             
         root.mainloop()                                                                 # Inicia o loop principal de eventos do Tkinter
 
     def tela_root(self):                                                                # Configurações da janela principal
-        self.root.title("Métodos de linhas de Transmissão")                             # Define o título da janela
+        self.root.title("LinhaMestre")                                                  # Define o título da janela
         self.root.geometry("700x500")                                                   # Define as dimensões iniciais da janela (largura x altura)
         self.root.resizable(False,False)                                                # Impede o redimensionamento da janela (largura, altura)
         self.root.maxsize(width=900, height=700)                                        # Define as dimensões máximas permitidas para a janela
@@ -22,9 +23,25 @@ class aplicativo():                                                             
                                      highlightbackground= 'black', highlightthickness=3 ) # Define borda e realce do frame
         self.frame_principal.place(relx= 0.02 , rely=0.02, relwidth= 0.96,relheight= 0.96) # Posiciona o frame principal na janela
     
-    def widgets_frame_principal(self):                                                 # Cria e posiciona os widgets no frame principal
+    def widgets_frame_principal(self):                                                  # Cria e posiciona os widgets no frame principal
+        #Botão de OK
         self.botao_ok = Button(self.frame_principal, text='OK', bd=4,                  # Cria o botão "OK"
-                                    font=('Arial',15))                                 # Define a fonte do botão
+                                     font=('Arial',15))                                 # Define a fonte do botão
         self.botao_ok.place(relx=.8,rely=.85,relwidth=0.15,relheight=0.1)              # Posiciona o botão
 
-aplicativo()                                                                           # Inicia o aplicativo, criando uma instância da classe
+        #Label de seleção
+        self.lb_variaveis = Label(self.frame_principal, text='Selecione o método:')    # Cria a label para seleção do método
+        self.lb_variaveis.place(relx=.05,rely=.32)                                      # Posiciona a label
+
+        # Lista de seleção dos métodos
+        self.Tipvar = StringVar()                                                       # Cria uma variável para armazenar a opção selecionada
+        self.TipV = ("Longitudinal Imagem", "Longitudinal Carson transposição",         # Define as opções do menu dropdown
+                      "Longitudinal Carson para-raio", "Longitudinal Carson feixe de condutor",
+                      "Longitudinal Carson impedância de sequência", "Transversal imagem",
+                      "Transversal transposição", "Transversal para-raio",
+                      "Transversal feixe de condutor", "Transversal capacitância de sequências")
+        self.Tipvar.set("Longitudinal Imagem")                                          # Define a opção inicial do menu dropdown
+        self.popupMenu = OptionMenu(self.frame_principal,self.Tipvar, *self.TipV)      # Cria o menu dropdown (OptionMenu)
+        self.popupMenu.place(relx=.25,rely=.3,relwidth=0.5,relheight=0.08)              # Posiciona o menu dropdown
+
+aplicativo()                                                                            # Inicia o aplicativo, criando uma instância da classe
