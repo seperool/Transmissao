60935bfc213048e4042c53519dff3b70d158e420
seperool <seperool@gmail.com>
2025-07-02 19:42:28 -0300

    add comentários

--- Modelos_python/longitudinais/Carson_para-raio.py
+++ Modelos_python/longitudinais/Carson_para-raio.py
@@ -2,140 +2,94 @@ import unittest
 import numpy as np
 import math
 
-# A função metodo_carson_para_raio (seu código) deve estar aqui
-# ou ser importada de um módulo. Para este exemplo, a estou colando aqui para completude.
 def metodo_carson_para_raio(ra, rb, rc, rp, xa, xb, xc, xp, ha, hb, hc, hp, rho, R=None, Rmg_val=None):
     """
-    Calcula a impedância longitudinal de uma linha de transmissão trifásica
-    considerando a presença de um cabo para-raios (cabo de guarda),
-    utilizando o Método de Carson e a redução de Kron.
+    Calcula a impedância longitudinal de uma linha de transmissão trifásica com cabo para-raios,
+    usando o Método de Carson e a redução de Kron.
 
-    A função retorna a matriz de impedância de fase (3x3) já considerando
-    o efeito de acoplamento e eliminação do cabo para-raios.
+    Retorna a matriz de impedância de fase (3x3) em Ohm/m, com o efeito do para-raios incorporado.
 
     Parâmetros:
-    ra, rb, rc (float): Resistências CA dos condutores das fases A, B e C (em Ohm/m).
-    rp (float): Resistência CA do condutor do cabo para-raios (em Ohm/m).
-    xa, xb, xc (float): Coordenadas horizontais (X) dos condutores A, B e C (em metros).
-    xp (float): Coordenada horizontal (X) do condutor do cabo para-raios (em metros).
-    ha, hb, hc (float): Coordenadas verticais (H) dos condutores A, B e C (em metros).
-    hp (float): Coordenada vertical (H) do condutor do cabo para-raios (em metros).
-                As alturas devem ser positivas.
-    rho (float): Resistividade do solo (em Ohm.m).
-    R (float, opcional): Raio físico do condutor (em metros). Se fornecido e Rmg_val não for,
-                         o RMG será calculado como R * exp(-1/4). Assume-se o mesmo R para todos.
-    Rmg_val (float, opcional): Raio Médio Geométrico (RMG) dos condutores (em metros).
-                               Se fornecido, tem prioridade sobre o cálculo a partir de 'R'.
-                               Assume-se o mesmo RMG para todos os condutores (fases e para-raios).
-
-    Retorna:
-    numpy.ndarray: Matriz de impedância de fase (3x3) da linha (em Ohm/m),
-                   com o efeito do cabo para-raios já incorporado via redução de Kron.
+    ra, rb, rc (float): Resistências CA das fases A, B, C (Ohm/m).
+    rp (float): Resistência CA do para-raios (Ohm/m).
+    xa, xb, xc, xp (float): Coordenadas horizontais (X) dos condutores (metros).
+    ha, hb, hc, hp (float): Coordenadas verticais (H) dos condutores (metros). Devem ser positivas.
+    rho (float): Resistividade do solo (Ohm.m).
+    R (float, opcional): Raio físico do condutor (metros). Usado para calcular RMG se Rmg_val não for dado.
+    Rmg_val (float, opcional): Raio Médio Geométrico (RMG) dos condutores (metros). Prioritário sobre 'R'.
+                                Assume-se o mesmo RMG para todos os condutores.
     """
     
-    # --- Constantes Físicas e Elétricas ---
-    f = 60                                       # Frequência do sistema (Hz).
-    mi_0 = 4 * math.pi * (10**(-7))              # Permeabilidade magnética do vácuo (H/m).
-    w = 2 * math.pi * f                          # Frequência angular (rad/s).
+    # --- Constantes Físicas ---
+    f = 60                       # Frequência do sistema (Hz).
+    mi_0 = 4 * math.pi * (10**(-7)) # Permeabilidade magnética do vácuo (H/m).
+    w = 2 * math.pi * f          # Frequência angular (rad/s).
 
-    # --- Cálculo e Validação do Raio Médio Geométrico (RMG) ---
-    Rmg = None # Inicializa RMG como None para verificar se foi calculado ou fornecido
+    # --- Cálculo/Validação do Raio Médio Geométrico (RMG) ---
+    Rmg = None 
     if Rmg_val is not None:
-        Rmg = Rmg_val # Se o RMG já foi fornecido, usa-o diretamente
+        Rmg = Rmg_val
     elif R is not None:
-        # Se o raio físico (R) foi fornecido, calcula o RMG.
-        # Para condutores sólidos ou simples, RMG = R * e^(-1/4).
-        # Para condutores trançados (como ACSR), o RMG geralmente é um valor tabelado pelo fabricante.
-        Rmg = R * math.exp(-1/4)
+        Rmg = R * math.exp(-1/4) # Cálculo padrão de RMG a partir do raio físico.
     
     # --- Validações de Entrada ---
-    # É crucial validar 'rho' antes de usá-lo na função math.sqrt para evitar 'math domain error'.
     if rho <= 0:
         raise ValueError("ERRO! A resistividade do solo (rho) deve ser um valor positivo.")
-
-    # Estas verificações garantem que os valores de entrada são válidos para o cálculo.
     if Rmg is None:
         raise ValueError("ERRO! É necessário fornecer o raio do condutor (R) OU o Raio Médio Geométrico (Rmg_val).")
     if Rmg <= 0:
         raise ValueError("ERRO! O Raio Médio Geométrico (RMG) deve ser um valor positivo.")
-    if ha <= 0 or hb <= 0 or hc <= 0 or hp <= 0: # Inclui a altura do cabo para-raios na validação
-        # As alturas devem ser positivas, pois o método de Carson assume condutores acima do solo.
+    if ha <= 0 or hb <= 0 or hc <= 0 or hp <= 0:
         raise ValueError("ERRO! As alturas de TODOS os condutores (Ha, Hb, Hc, Hp) devem ser maiores que zero.")
 
-    # --- Termos de Correção do Método de Carson para o Solo ---
-    # rd: Termo de resistência de Carson (Ohms/m).
-    rd = 9.869 * (10**(-7)) * f                  # Ohms/m.
+    # --- Termos de Correção do Método de Carson ---
+    rd = 9.869 * (10**(-7)) * f    # Termo de resistência de Carson (Ohms/m).
+    De = 659 * (math.sqrt(rho / f)) # Distância de retorno equivalente do solo (metros).
 
-    # De: Distância de retorno equivalente do solo (metros).
-    De = 659 * (math.sqrt(rho / f))              # Metros
+    # --- Cálculo das Distâncias Geométricas ---
+    # Distâncias euclidianas 2D entre condutores reais.
+    dab = np.sqrt(((xa - xb)**2) + ((ha - hb)**2))
+    dac = np.sqrt(((xa - hc)**2) + ((ha - hc)**2))
+    dbc = np.sqrt(((xb - xc)**2) + ((hb - hc)**2))
+    dap = np.sqrt(((xa - xp)**2) + ((ha - hp)**2))
+    dbp = np.sqrt(((xb - xp)**2) + ((hb - hp)**2))
+    dcp = np.sqrt(((xc - xp)**2) + ((hc - hp)**2))
 
-    # --- Cálculo das Distâncias Geométricas entre os condutores ---
-    # As distâncias entre os centros dos condutores reais são calculadas usando a fórmula da distância euclidiana 2D.
-    dab = np.sqrt(((xa - xb)**2) + ((ha - hb)**2)) # Distância entre condutores A e B
-    dac = np.sqrt(((xa - xc)**2) + ((ha - hc)**2)) # Distância entre condutores A e C
-    dbc = np.sqrt(((xb - xc)**2) + ((hb - hc)**2)) # Distância entre condutores B e C
-    
-    # Distâncias entre as fases e o cabo para-raios (p)
-    dap = np.sqrt(((xa - xp)**2) + ((ha - hp)**2)) # Distância entre A e para-raios
-    dbp = np.sqrt(((xb - xp)**2) + ((hb - hp)**2)) # Distância entre B e para-raios
-    dcp = np.sqrt(((xc - xp)**2) + ((hc - hp)**2)) # Distância entre C e para-raios
-
-    # --- Cálculo das Impedâncias Próprias e Mútuas por Unidade de Comprimento (Ohm/m) ---
-    # As fórmulas de Carson resultam em impedâncias por metro.
-    # Impedâncias Próprias (Zii): Representam a impedância de um condutor em relação ao retorno pelo solo.
-    # Zii = Ri_condutor + R_terra_Carson + j * X_terra_propria_Carson
-    # R_terra_Carson é 'rd'. X_terra_propria_Carson é a parte logarítmica com De/RMG.
+    # --- Cálculo das Impedâncias Próprias e Mútuas (Ohm/m) ---
+    # Zii: Impedâncias próprias (condutor para retorno no solo).
     Zaa = ra + rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / Rmg)
     Zbb = rb + rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / Rmg)
     Zcc = rc + rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / Rmg)
-    Zpp = rp + rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / Rmg) # Impedância própria do cabo para-raios
+    Zpp = rp + rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / Rmg)
 
-    # Impedâncias Mútuas (Zij): Representam o acoplamento eletromagnético entre dois condutores,
-    # considerando o retorno pelo solo.
-    # Zij = R_terra_Carson + j * X_terra_mutua_Carson
-    # R_terra_Carson é 'rd'. X_terra_mutua_Carson é a parte logarítmica com De/dij.
-    Zab = rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / dab) # Mútua entre A e B
-    Zac = rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / dac) # Mútua entre A e C
-    Zbc = rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / dbc) # Mútua entre B e C
-    
-    # Impedâncias mútuas entre as fases e o cabo para-raios (p)
-    Zap = rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / dap) # Mútua entre A e para-raios
-    Zbp = rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / dbp) # Mútua entre B e para-raios
-    Zcp = rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / dcp) # Mútua entre C e para-raios
+    # Zij: Impedâncias mútuas (acoplamento entre dois condutores e retorno no solo).
+    Zab = rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / dab)
+    Zac = rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / dac)
+    Zbc = rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / dbc)
+    Zap = rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / dap)
+    Zbp = rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / dbp)
+    Zcp = rd + ((1j * w * mi_0) / (2 * math.pi)) * math.log(De / dcp)
 
-    # As impedâncias mútuas são simétricas na matriz de impedância (Zij = Zji).
+    # Assimetria natural das mútuas na matriz de impedância (Zij = Zji).
     Zba = Zab
     Zca = Zac
     Zcb = Zbc
-    Zpa = Zap # Mútua entre para-raios e A
-    Zpb = Zbp # Mútua entre para-raios e B
-    Zpc = Zcp # Mútua entre para-raios e C
+    Zpa = Zap
+    Zpb = Zbp
+    Zpc = Zcp
 
     # --- Construção das Submatrizes para Redução de Kron ---
-    # Z_1: Submatriz de impedâncias próprias e mútuas entre as fases (Zff) - 3x3
-    Z_1 = np.array([[Zaa, Zab, Zac],
-                    [Zba, Zbb, Zbc],
-                    [Zca, Zcb, Zcc]])
+    Z_1 = np.array([[Zaa, Zab, Zac], [Zba, Zbb, Zbc], [Zca, Zcb, Zcc]]) # Impedâncias fase-fase (Zff)
+    Z_2 = np.array([[Zap], [Zbp], [Zcp]])                               # Impedâncias fase-para-raios (Zfp)
+    Z_3 = np.array([[Zpa, Zpb, Zpc]])                                   # Impedâncias para-raios-fase (Zpf)
+    Z_4 = np.array([[Zpp]])                                             # Impedância para-raios-para-raios (Zpp)
 
-    # Z_2: Submatriz de impedâncias mútuas entre as fases e o cabo para-raios (Zfp) - 3x1
-    Z_2 = np.array([[Zap],
-                    [Zbp],
-                    [Zcp]])
-
-    # Z_3: Submatriz de impedâncias mútuas entre o cabo para-raios e as fases (Zpf) - 1x3
-    Z_3 = np.array([[Zpa, Zpb, Zpc]])
-
-    # Z_4: Submatriz de impedância própria do cabo para-raios (Zpp) - 1x1
-    Z_4 = np.array([[Zpp]])
-
-    # --- Redução de Kron para Eliminação do Cabo Para-Raios ---
-    # A fórmula é: Z_p = Z_1 - (Z_2 @ Z_4_inversa @ Z_3)
-    # Como Z_4 é 1x1, Z_4_inversa é 1/Z_4[0,0].
-    # Usamos np.dot para multiplicação de matrizes.
+    # --- Redução de Kron para Eliminação do Para-Raios ---
+    # Fórmula: Z_reduzida = Z_1 - (Z_2 @ inv(Z_4) @ Z_3)
+    # inv(Z_4) é 1/Z_4[0,0] para matriz 1x1.
     termo_correcao = np.dot(Z_2, Z_3) / Z_4[0,0]
 
-    # Calcula a impedância final da matriz de fase com o efeito do cabo para-raios.
-    Zp = Z_1 - termo_correcao
+    Zp = Z_1 - termo_correcao # Matriz de impedância de fase final.
 
     return Zp
 
