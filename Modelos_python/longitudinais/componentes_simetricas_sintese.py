e6f46479b46be732f49bd62e37b507867c8d4dd9
seperool <seperool@gmail.com>
2025-07-07 21:05:37 -0300

    Organizando metodos

---
+++ Modelos_python/longitudinais/componentes_simetricas_sintese.py
@@ -0,0 +1,2 @@
+import numpy as np # Importa a biblioteca NumPy para operações com arrays e matrizes, especialmente úteis para números complexos
+import math # Importa o módulo math para funções matemáticas como pi, sqrt e log
